1e041241217d774d2222ef78d51549ec76852aae
Azur Spenle <aspenle@k1r3p6.42mulhouse.fr>
2026-04-21 14:41:16 +0200

    final docstringv2

--- src/utils.py
+++ src/utils.py
@@ -21,6 +21,20 @@ class stats_deco(ABC):
         return cls.all_monitored
 
     def __get__(self, instance: Any, owner: Any) -> Callable:
+        """bind self to methode
+
+        Args:
+            instance (Any):
+            instance of the class that \
+            the attribute is accessed on (or None if accessed via the class)
+            owner (Any):
+                the class that defines the descriptor
+
+        Returns:
+            Callable:
+            a bound callable where the instance \
+                is implicitly passed as the first argument
+        """
         if instance is None:
             return self
 
@@ -34,11 +48,21 @@ class stats_deco(ABC):
 
 class monitor_time(stats_deco):
     def __init__(self, func: Callable):
+        """init the decorator
+
+        Args:
+            func (Callable): function to bounc to
+        """
         super().__init__(func)
         self._stats["total_time"] = 0
         self._stats["call_count"] = 0
 
     def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        """call decored function and add time spent
+
+        Returns:
+            Any: function result
+        """
         start = perf_counter()
         try:
             result = self.func(*args, **kwargs)
@@ -59,6 +83,33 @@ def format_dict(
         reverse: bool = True,
         value_filter: Optional[Callable[[Any], bool]] = None
         ) -> Tuple[Tuple[Any, Any], ...]:
+    """format specified entry of a dict as a tuple of \
+    (key, value) with optional filter and sort
+
+    Args:
+        main (dict):
+            source dictionary to extract values from
+        *keys (Any):
+            keys to extract from the dictionary
+
+        sort (bool, optional):
+            whether to sort the resulting tuples. Defaults to True.
+        sort_key (Optional[Callable[[tuple[Any, Any]], Any]], optional):
+            function used to compute sorting key for each (key, value) pair. \
+        Defaults to None.
+        reverse (bool, optional):
+            whether to sort in descending order. \
+        Defaults to True.
+        value_filter (Optional[Callable[[Any], bool]], optional):
+            function that filters values; \
+        only values returning True are included. \
+        Defaults to None.
+
+    Returns:
+        Tuple[Tuple[Any, Any], ...]:
+            tuple of (key, value) pairs extracted from the dictionary, \
+        optionally filtered and sorted
+    """
     result: tuple[tuple[Any, Any], ...] = tuple(
         (key, value)
         for key in keys
